ddaba0852350d2866543f48bb0f7969c3735b991
Priyanka Nikam <priyanka.nikam@bgauss.com>
2026-03-23 18:05:08 +0530

    Implement like and share feature

---
+++ BGaussCRM.API/Models/UserLike.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BGaussCRM.API.Models;
+
+public partial class UserLike
+{
+    public int Id { get; set; }
+
+    public string UserId { get; set; } = null!;
+
+    public int ScootyId { get; set; }
+
+    public DateTime LikedAt { get; set; }
+
+    public virtual ScootyInventory Scooty { get; set; } = null!;
+}
